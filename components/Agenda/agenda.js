314cd31f74a90e50b843c6f1a7bdd950de9d04f3
Ayush Kumar Namdeo <ayushnamdeo.an@gmail.com>
2022-12-27 23:09:31 +0530

    timer

--- components/Agenda/agenda.js
+++ components/Agenda/agenda.js
@@ -1,32 +1,32 @@
 export const agenda = [
   {
     Time: "09:00 - 09:30",
-    event: "Opening ceremony",
+    event: "Coming Soon",
     id: 1,
   },
   {
     Time: "09:30 - 10:00",
-    event: "Quantum mechanical concepts for quantum computing",
+    event: "Coming Soon",
     id: 2,
   },
   {
     Time: "10:00 - 10:30",
-    event: "Intro to Quantum Computing using Qbraid",
+    event: "Coming Soon",
     id: 3,
   },
   {
     Time: "10:30 - 11:00",
-    event: "Let's Discover Quantum Computing with Qiskit!",
+    event: "Coming Soon",
     id: 4,
   },
   {
     Time: "11:00 - 11:30",
-    event: "Industrial Applications of Quantum Computing",
+    event: "Coming Soon",
     id: 5,
   },
   {
     Time: "11:30 - 12:00",
-    event: "Cryptography in Quantum Computing",
+    event: "Coming Soon",
     id: 6,
   },
   {
@@ -35,12 +35,12 @@ export const agenda = [
   },
   {
     Time: "14:00 - 16:00",
-    event: "Workshop: Quantum Random Number Generation",
+    event: "Coming Soon",
     id: 8,
   },
   {
     Time: "14:00 - 16:00",
-    event: "Workshop: Quantum Key Distribution",
+    event: "Coming Soon",
     id: 9,
   },
 ];
