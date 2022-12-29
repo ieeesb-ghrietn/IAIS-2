9dff8ddbd0cd15f6a9b6386bc0e76eb4796bbafa
Ayush Kumar Namdeo <ayushnamdeo.an@gmail.com>
2022-12-29 23:50:30 +0530

    committee

---
+++ components/Committee/data.js
@@ -0,0 +1,53 @@
+import Speaker1 from "../../images/speakers/speaker.png";
+import Speaker2 from "../../images/speakers/speaker.png";
+import Speaker3 from "../../images/speakers/speaker.png";
+import Speaker4 from "../../images/speakers/speaker.png";
+import Speaker5 from "../../images/speakers/speaker.png";
+import Speaker6 from "../../images/speakers/speaker.png";
+import Speaker7 from "../../images/speakers/speaker.png";
+import Speaker8 from "../../images/speakers/speaker.png";
+export const speakers = [
+  {
+    image: Speaker1,
+    name: "Coming Soon",
+    profession: "Coming Soon",
+  },
+  {
+    image: Speaker2,
+    name: "Coming Soon",
+    profession: "Coming Soon",
+  },
+  {
+    image: Speaker3,
+    name: "Coming Soon",
+    profession: "Coming Soon",
+  },
+  {
+    image: Speaker4,
+    name: "Coming Soon",
+    profession:
+      "Coming Soon ",
+  },
+  {
+    image: Speaker5,
+    name: "Coming Soon",
+    profession: "Coming Soon",
+  },
+  {
+    image: Speaker6,
+    name: "Coming Soon",
+    profession: "Coming Soon",
+  },
+  {
+    image: Speaker7,
+    name: "Coming Soon",
+    profession:
+      "Coming Soon",
+  },
+  {
+    image: Speaker8,
+    name: "Coming Soon",
+    profession:
+      "Coming Soon",
+  },
+];
