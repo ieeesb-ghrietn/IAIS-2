de6ccd537b2879c2f1893a90a715303f8ae820f7
Ayush Kumar Namdeo <ayushnamdeo.an@gmail.com>
2022-12-29 23:24:24 +0530

    change

--- components/Committee/Index.jsx
+++ components/Committee/Index.jsx
@@ -1,4 +1,4 @@
-import { committee } from "./committee";
+import { Committee } from "./Committee";
 const Committee = () =>{
     return (
         <section id='committee' className="relative section-container">
