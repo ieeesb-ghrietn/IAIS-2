3a8c804b31d053bd1be8acfd869055a0eaf330bc
Ayush Kumar Namdeo <ayushnamdeo.an@gmail.com>
2022-12-30 22:04:16 +0530

    check

--- components/Committee/Index.jsx
+++ components/Committee/Index.jsx
@@ -36,7 +36,65 @@ const Committee = () =>{
       <div className="hidden lg:block absolute bottom-[4rem] left-0 lg:bottom-10 lg:right-1/4 lg:w-[80px] 2xl:w-[150px]">
         <Image src={atom} alt="atombotton" />
       </div>
+      
 
+<div class="wrapper">
+    <h1>Our Team</h1>
+    <div class="our_team">
+        <div class="team_member">
+          <div class="member_img">
+             <Image src="https://i.imgur.com/2Necikc.png" alt="our_team"/>
+            <div class="social_media">
+               <div class="facebook item"><i class="fab fa-facebook-f"></i></div>
+               <div class="twitter item"><i class="fab fa-twitter"></i></div>
+               <div class="instagram item"><i class="fab fa-instagram"></i></div>
+             </div>
+          </div>
+          <h3>john wright</h3>
+          <span>CEO</span>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione perspiciatis, error deleniti quaerat beatae doloribus incidunt excepturi. Fugit deleniti accusantium neque hic quidem voluptatibus cumque.</p>
+        </div>
+        <div class="team_member">
+           <div class="member_img">
+             <Image src="https://i.imgur.com/JzUIF4o.png" alt="our_team"/>
+             <div class="social_media">
+               <div class="facebook item"><i class="fab fa-facebook-f"></i></div>
+               <div class="twitter item"><i class="fab fa-twitter"></i></div>
+               <div class="instagram item"><i class="fab fa-instagram"></i></div>
+             </div>
+          </div>
+          <h3>barbara mori</h3>
+          <span>Accountant</span>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione perspiciatis, error deleniti quaerat beatae doloribus incidunt excepturi. Fugit deleniti accusantium neque hic quidem voluptatibus cumque.</p>
+      </div>
+        <div class="team_member">
+           <div class="member_img">
+             <Image src="https://i.imgur.com/Ctwf8HA.png" alt="our_team"/>
+             <div class="social_media">
+               <div class="facebook item"><i class="fab fa-facebook-f"></i></div>
+               <div class="twitter item"><i class="fab fa-twitter"></i></div>
+               <div class="instagram item"><i class="fab fa-instagram"></i></div>
+             </div>
+          </div>
+          <h3>harry dickens</h3>
+          <span>Product Manager</span>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione perspiciatis, error deleniti quaerat beatae doloribus incidunt excepturi. Fugit deleniti accusantium neque hic quidem voluptatibus cumque.</p>
+      </div>
+        <div class="team_member">
+           <div class="member_img">
+             <Image src="https://i.imgur.com/A1Fjq0d.png" alt="our_team"/>
+             <div class="social_media">
+               <div class="facebook item"><i class="fab fa-facebook-f"></i></div>
+               <div class="twitter item"><i class="fab fa-twitter"></i></div>
+               <div class="instagram item"><i class="fab fa-instagram"></i></div>
+             </div>
+          </div>
+          <h3>sammy louise</h3>
+          <span>product analyst</span>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione perspiciatis, error deleniti quaerat beatae doloribus incidunt excepturi. Fugit deleniti accusantium neque hic quidem voluptatibus cumque.</p>
+      </div>  
+    </div>
+</div>
  
         </section>
 
