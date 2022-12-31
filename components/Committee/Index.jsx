57e52849d209e9ff537351c0db53322cda55012e
Ayush Kumar Namdeo <ayushnamdeo.an@gmail.com>
2022-12-31 11:39:25 +0530

    committee

--- components/Committee/Index.jsx
+++ components/Committee/Index.jsx
@@ -8,6 +8,8 @@ import vivekkapur from "../../images/teams/vivekkapur.jpg";
 import smitanirkhi from "../../images/teams/smitanirkhi.jpg";
 import tabassumkhan from "../../images/teams/tabassumkhan.jpg";
 import shardachabbria from "../../images/teams/shardachabbria.jpg";
+import Ravindranath from "../../images/teams/ravindranath.jpg";
+import Linkedin from "../../images/teams/linkedin.png";
 import { useEffect } from "react";
 const Committee = () =>{
     
@@ -42,6 +44,82 @@ const Committee = () =>{
         <Image src={atom} alt="atombotton" />
       </div>
       
+      {/*Added code*/}
+
+      <section class="u-align-center u-clearfix u-white u-section-1" id="carousel_4665">
+      <div class="u-clearfix u-sheet u-sheet-1">
+        <h2 class="u-custom-font u-font-montserrat u-text u-text-body-color u-text-default u-text-1">Our Mentors</h2>
+        <div class="u-list u-list-1">
+          <div class="u-repeater u-repeater-1">
+            <div class="u-container-style u-list-item u-repeater-item u-white u-list-item-1">
+              <div class="u-container-layout u-similar-container u-container-layout-1">
+                <Image alt="" class="u-expanded-width u-image u-image-default u-image-1" data-image-width="626" data-image-height="417" src={vivekkapur}/>
+                <div class="u-align-center u-container-style u-group u-white u-group-1">
+                  <div class="u-container-layout u-container-layout-2">
+                    <h4 class="u-text u-text-2">Dr. Vivek Kapur</h4>
+                    <h6 class="u-text u-text-3">Director(GHRIETN)</h6>
+                  </div>
+                </div><span class="u-file-icon u-icon u-icon-circle u-palette-1-base u-text-white u-icon-1"><Image src={Linkedin} alt=""/></span>
+              </div>
+            </div>
+            <div class="u-container-style u-list-item u-repeater-item u-video-cover u-white">
+              <div class="u-container-layout u-similar-container u-container-layout-3">
+                <Image alt="" class="u-expanded-width u-image u-image-default u-image-2" data-image-width="626" data-image-height="417" src={smitanirkhi}/>
+                <div class="u-align-center u-container-style u-group u-white u-group-2">
+                  <div class="u-container-layout u-container-layout-4">
+                    <h4 class="u-text u-text-4">Dr.Smita Nirkhi</h4>
+                    <h6 class="u-text u-text-5">HOD AI</h6>
+                  </div>
+                </div><span class="u-file-icon u-icon u-icon-circle u-palette-1-base u-text-white u-icon-2"><Image src={Linkedin} alt=""/></span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="u-align-center u-clearfix u-white u-section-2" id="carousel_809d">
+      <div class="u-clearfix u-sheet u-sheet-1">
+        <h2 class="u-custom-font u-font-montserrat u-text u-text-body-color u-text-default u-text-1">Coordinators</h2>
+        <div class="u-expanded-width u-list u-list-1">
+          <div class="u-repeater u-repeater-1">
+            <div class="u-container-style u-list-item u-repeater-item u-white u-list-item-1">
+              <div class="u-container-layout u-similar-container u-container-layout-1">
+                <Image alt="" class="u-expanded-width u-image u-image-default u-image-1" data-image-width="626" data-image-height="417" src={tabassumkhan}/>
+                <div class="u-align-center u-container-style u-group u-white u-group-1">
+                  <div class="u-container-layout u-container-layout-2">
+                    <h4 class="u-text u-text-2">Prof. Tabassum Khan</h4>
+                    <h6 class="u-text u-text-3">Coordinator</h6>
+                  </div>
+                </div><span class="u-file-icon u-icon u-icon-circle u-palette-1-base u-text-white u-icon-1"><Image src={Linkedin} alt=""/></span>
+              </div>
+            </div>
+            <div class="u-container-style u-list-item u-repeater-item u-video-cover u-white">
+              <div class="u-container-layout u-similar-container u-container-layout-3">
+                <Image alt="" class="u-expanded-width u-image u-image-default u-image-2" data-image-width="626" data-image-height="417" src={shardachabbria}/>
+                <div class="u-align-center u-container-style u-group u-white u-group-2">
+                  <div class="u-container-layout u-container-layout-4">
+                    <h4 class="u-text u-text-4">Dr. Sharda Chabbria</h4>
+                    <h6 class="u-text u-text-5">Co-Coordinator</h6>
+                  </div>
+                </div><span class="u-file-icon u-icon u-icon-circle u-palette-1-base u-text-white u-icon-2"><Image src={Linkedin} alt=""/></span>
+              </div>
+            </div>
+            <div class="u-container-style u-list-item u-repeater-item u-video-cover u-white">
+              <div class="u-container-layout u-similar-container u-container-layout-5">
+                <Image alt="" class="u-expanded-width u-image u-image-default u-image-3" data-image-width="626" data-image-height="417" src={Ravindranath}/>
+                <div class="u-align-center u-container-style u-group u-white u-group-3">
+                  <div class="u-container-layout u-container-layout-6">
+                    <h4 class="u-text u-text-6">Mr. Ravindranath Sawane</h4>
+                    <h6 class="u-text u-text-7">Student Coordinator</h6>
+                  </div>
+                </div><span class="u-file-icon u-icon u-icon-circle u-palette-1-base u-text-white u-icon-3"><Image src={Linkedin} alt=""/></span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    
 			  </section>
 
     )
